304faf1bdca26c80a74ac4f4c4499bcaa5ffd8aa
MigueMartelo <miguemartelo1031@gmail.com>
2019-01-18 15:45:25 -0500

    Updates

---
+++ precache-manifest.7ec33960fc449655156af410ad8d9efe.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5869347af52e85f147b6",
+    "url": "/buscadorimagenes/static/js/runtime~main.5869347a.js"
+  },
+  {
+    "revision": "29f42bfecfff7fe2172f",
+    "url": "/buscadorimagenes/static/js/main.29f42bfe.chunk.js"
+  },
+  {
+    "revision": "e74b4aa2d6132e8df6e6",
+    "url": "/buscadorimagenes/static/js/1.e74b4aa2.chunk.js"
+  },
+  {
+    "revision": "c8e6d3c829d0c216c7632425a03c9f41",
+    "url": "/buscadorimagenes/index.html"
+  }
+];
